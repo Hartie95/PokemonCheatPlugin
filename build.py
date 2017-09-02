5137384706abd2de1c43db5c10152f3961853d1c
kotaro <k0uta120u@icloud.com>
2017-09-02 22:26:36 +0900

    Reformat appearance

--- build.py
+++ build.py
@@ -38,10 +38,30 @@ if os.path.exists(''):
 else:
     run("mkdir bin")
 
-run(CC+  " -Os -s  -g -I include -I include/libntrplg " + allFile('source/libntrplg/*.c') + allFile('source/ns/*.c') + allFile('source/*.c') + allFile('source/battle/*.c') + allFile('source/rng/*.c') + allFile('source/libctru/*.c') + " -c  -march=armv6 -mlittle-endian  ");
-run(CC+"  -Os " + allFile('source/libntrplg/*.s') +  allFile('source/ns/*.s')  + allFile('source/*.s') + allFile('source/libctru/*.s') + " -c -s -march=armv6 -mlittle-endian ");
+run(CC + 
+    " -Os -s  -g -I include -I include/libntrplg " + 
+    allFile('source/libntrplg/*.c')                +
+    allFile('source/ns/*.c')                       +
+    allFile('source/*.c')                          +
+    allFile('source/battle/*.c')                   +
+    allFile('source/rng/*.c')                      +
+    allFile('source/libctru/*.c')                  +
+    " -c  -march=armv6 -mlittle-endian")
 
-run(LD + ' ' + LIBPATH + " -pie --print-gc-sections  -T 3ds.ld -Map=homebrew.map " + allFile("*.o") + " " + allFile("lib/*.o") + " " + allFile("lib/*.a") + " -lc --nostdlib")
+run(CC                              +
+    "  -Os "                        +
+    allFile('source/libntrplg/*.s') +
+    allFile('source/ns/*.s')        +
+    allFile('source/*.s')           +
+    allFile('source/libctru/*.s')   +
+    " -c -s -march=armv6 -mlittle-endian ")
+
+run(LD + ' ' + LIBPATH + " -pie --print-gc-sections  -T 3ds.ld -Map=homebrew.map " + 
+    allFile("*.o") + " "     + 
+    allFile("lib/*.o") + " " + 
+    allFile("lib/*.a")       + 
+    " -lc --nostdlib")
+
 run("cp -r *.o obj/ ")
 run("cp a.out bin/homebrew.elf ")
 run(OC+" -O binary a.out payload.bin -S")
