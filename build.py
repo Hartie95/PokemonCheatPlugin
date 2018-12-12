1cad18818eaf64b97b6bddb2945dfb82b8103cc5
0NONE <0none@cumallover.me>
2018-12-12 21:15:17 +0100

    Fixed folders not being created when building repo

--- build.py
+++ build.py
@@ -26,10 +26,16 @@ def run(cmd):
 	#print(cmd);
 	os.system(cmd)
 
+# Check if required folders exist
+if not os.path.exists("obj"):
+    os.makedirs("obj");
+if not os.path.exists("bin"):
+    os.makedirs("bin");
+
 cwd = os.getcwd()
 run("rm obj/*.o")
 run("rm bin/*.elf")
-run(CC+  " -Os -s  -g -I include -I include/libntrplg " + allFile('source/libntrplg/*.c') + allFile('source/ns/*.c') + allFile('source/*.c') + allFile('source/battle/*.c') + allFile('source/rng/*.c') + allFile('source/libctru/*.c') + " -c  -march=armv6 -mlittle-endian  ");
+print(CC+  " -Os -s  -g -I include -I include/libntrplg " + allFile('source/libntrplg/*.c') + allFile('source/ns/*.c') + allFile('source/*.c') + allFile('source/battle/*.c') + allFile('source/rng/*.c') + allFile('source/libctru/*.c') + " -c  -march=armv6 -mlittle-endian ");
 run(CC+"  -Os " + allFile('source/libntrplg/*.s') +  allFile('source/ns/*.s')  + allFile('source/*.s') + allFile('source/libctru/*.s') + " -c -s -march=armv6 -mlittle-endian ");
 
 run(LD + ' ' + LIBPATH + " -pie --print-gc-sections  -T 3ds.ld -Map=homebrew.map " + allFile("*.o") + " " + allFile("lib/*.o") + " " + allFile("lib/*.a") + " -lc --nostdlib")
